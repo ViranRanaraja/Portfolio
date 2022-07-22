703d1d063e2483a56e7cc647109ec9fb1b81063e
Viran Ranaraja <venuraka.20191253@iit.ac.lk>
2022-07-22 19:02:59 +0530

    Drawing Animation in Home Page fixed

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,7 +1,7 @@
 import './index.scss';
 import LogoS from '../../../assests/images/logo-v.png';
 import { useEffect, useRef } from 'react';
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin';
+import { DrawSVGPlugin } from "gsap-trial/DrawSVGPlugin"
 import gsap from 'gsap-trial';
 import React from 'react';
 
@@ -10,21 +10,26 @@ const Logo = () => {
     const bgRef = useRef();
     const outlineLogoRef = useRef();
     const solidLogoRef = useRef();
+    const didAnimate = useRef(false);
+    
 
     useEffect(() => {
       gsap.registerPlugin(DrawSVGPlugin)
 
-      gsap.timeline()
+      if (didAnimate.current) { return; }
+        didAnimate.current = true;
 
-      .to(bgRef.current, {
-          duration: 1,
-          opacity: 1,
-        })
+        gsap.timeline()
 
-      .from(outlineLogoRef.current, {
-          drawSVG: 0,
-          duration: 20,
-        });
+        .to(bgRef.current, {
+            duration: 1,
+            opacity: 1,
+          })
+
+        .from(outlineLogoRef.current, {
+            drawSVG: 0,
+            duration: 20,
+          });
 
       gsap.fromTo(
         solidLogoRef.current,
